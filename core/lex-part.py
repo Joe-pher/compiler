3871d88ca3e7b93b82fbf7e09f5b9a33b024cce6
Jo.sepholdman2@gmail.com <aLoVfP15-7X>
2019-11-03 15:31:11 +0330

    Part1 regular expressions added!

--- core/lex-part.py
+++ core/lex-part.py
@@ -1,37 +1,70 @@
 # ------------------------------------------------------------
-# calclex.py
+#   Compiler project fall 2019 - Phase1 : lexical Analyzer
 #
-# tokenizer for a simple expression evaluator for
-# numbers and +,-,*,/
+#   Parsa Farin nia, Seyyed Amirali Sajjadi
 # ------------------------------------------------------------
 import ply.lex as lex
 
-# List of token names.   This is always required
+# List of token names.
 tokens = (
-    'NUMBER',
-    'PLUS',
-    'MINUS',
-    'TIMES',
-    'DIVIDE',
-    'LPAREN',
-    'RPAREN',
+    'TOKEN_ID',
+    'TOKEN_INTEGER',
+    ''
 )
 
-# Regular expression rules for simple tokens
-t_PLUS = r'\+'
-t_MINUS = r'-'
-t_TIMES = r'\*'
-t_DIVIDE = r'/'
-t_LPAREN = r'\('
-t_RPAREN = r'\)'
+# Regular expression rules for tokens
+t_TOKEN_ID = r'_([A-Za-z0-9]_)*([A-Za-z0-9]{2})*|[A-Za-z]([A-Za-z0-9]{2})*((_[A-Za-z0-9])*|([A-Za-z0-9]_)*)([A-Za-z0-9]{2})*'
 
+#TODO: Comment token regex rule comes here!
 
-# A regular expression rule with some action code
-def t_NUMBER(t):
-    r'\d+'
-    t.value = int(t.value)
+# Regular expression rules for trivial tokens
+t_TOKEN_CLASS = r'class'
+t_TOKEN_REFERENCE = r'reference'
+t_TOKEN_STATIC = r'static'
+t_TOKEN_INT_TYPE = r'int'
+t_TOKEN_REAL_TYPE = r'real'
+t_TOKEN_BOOL_TYPE = r'bool'
+t_TOKEN_STRING_TYPE = r'string'
+t_TOKEN_VOID = r'void'
+t_TOKEN_TRUE = r'true'
+t_TOKEN_FALSE = r'false'
+t_TOKEN_PRINT = r'print'
+t_TOKEN_RETURN = r'return'
+t_TOKEN_BREAK = r'break'
+t_TOKEN_CONTINUE = 'continue'
+t_TOKEN_IF = r'if'
+t_TOKEN_ELSE = r'else'
+t_TOKEN_ELSEIF = r'elseif'
+t_TOKEN_WHILE = r'while'
+t_TOKEN_FOR = r'for'
+t_TOKEN_TO = r'to'
+t_TOKEN_IN = r'in'
+t_TOKEN_STEPS = r'steps'
+t_TOKEN_BITWISE_AND = r'&'
+t_TOKEN_AND = r'&&'
+# TODO: Part2 trivial regexes come here!
+
+# More complex tokens are defined here
+def t_TOKEN_INTEGER(t):
+    r'[\+-]?(0(x[1-9A-F]([0-9A-F])*|b1[01]*)?|[1-9]([0-9])*|0x|0b)'
+
+    sign = 1
+    string_value = t.value
+    if '-' in string_value:
+        sign = -1
+        string_value = string_value[1:]
+    elif '+' in string_value:
+        string_value = string_value[1:]
+    t.value = sign * int(string_value, 0)
     return t
-
+# TODO: Real_number,String functions definitions come here
+# Note that real number function must parse it's raw value (like '12.65') to it's actual value(a real number) (ex: real number token string value = '12.42' and t.value must be set to 12.42)
+# Note that string function must parse it's raw value to get its actual value and put it at t.value.
+# string parsing examples:
+#   string value                 value required to put in t.value
+#   '"hello"'         ->                'hello'
+#   '"hello\\n \\r"'  ->                'hello\n \r'
+#   'this '+' is '+' a '+ ' test!' ->   'this is a test!'
 
 # Define a rule so we can track line numbers
 def t_newline(t):
@@ -39,8 +72,8 @@ def t_newline(t):
     t.lexer.lineno += len(t.value)
 
 
-# A string containing ignored characters (spaces and tabs)
-t_ignore = ' \t'
+# A string containing ignored characters (White spaces except for new line (new line is dealt with above))
+t_ignore = ' \t\r'
 
 
 # Error handling rule
@@ -54,9 +87,8 @@ lexer = lex.lex()
 
 # Test it out
 data = '''
- 3 + 4 * 10
-   + -20 *2
- '''
+        _A_mirali 0b1101
+'''
 
 # Give the lexer some input
 lexer.input(data)
